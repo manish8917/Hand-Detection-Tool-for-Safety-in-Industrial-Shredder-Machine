e6538bcf8af697a53a0d19b8050078be8eca07c7
manish8917 <manishislampur1988@gmail.com>
2022-10-05 13:24:15 +0530

    Git Push

---
+++ Other preproceesing techniques/4. ImageSplitter/image-splitter.py
@@ -0,0 +1,14 @@
+# place this file inside the folder containing the images and run from there. It will separate entire images into 3 parts - training, testing & validation
+
+import splitfolders # or import split_folders
+
+# Split with a ratio.
+# To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
+splitfolders.ratio("input_folder", output="output",
+    seed=1337, ratio=(.8, .2), group_prefix=None, move=False) # default values
+
+# Split val/test with a fixed number of items, e.g. `(100, 100)`, for each set.
+# To only split into training and validation set, use a single number to `fixed`, i.e., `10`.
+# Set 3 values, e.g. `(300, 100, 100)`, to limit the number of training values.
+#splitfolders.fixed("input_folder", output="output",
+#    seed=1337, fixed=(100, 100), oversample=False, group_prefix=None, move=False) # default values
